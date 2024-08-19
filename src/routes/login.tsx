08dc187bf3824cb3b12278f3dfdc4923f10a0a42
Hyunchul <chc3954@gmail.com>
2024-08-19 11:18:11 -0400

    Added login feature

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,80 @@
+import { useState } from "react";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import {
+  Container,
+  Error,
+  Input,
+  Switcher,
+  Title,
+  Form,
+} from "../components/auth-components";
+
 export default function Login() {
-  return <>Login</>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    if (isLoading || email === "" || password === "") return;
+
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Container>
+      <Title>Login</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          onChange={onChange}
+          required
+        />
+        <Input
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          onChange={onChange}
+          required
+        />
+        <Input type="submit" value={isLoading ? "Loading..." : "Login"} />
+        {error && <Error>{error}</Error>}
+        <Switcher>
+          Don't have an account?{" "}
+          <Link to="/create-account">Create one &rarr;</Link>
+        </Switcher>
+      </Form>
+    </Container>
+  );
 }
